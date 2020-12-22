dab12613c5d8bc17229f550006ca048f07711f6b
Jeferson <jefersonborba@protonmail.com>
2020-12-22 01:36:43 -0300

    added react-router-dom, also styled the menu a little further

--- src/components/menu/style.js
+++ src/components/menu/style.js
@@ -5,15 +5,33 @@ export const Holder = styled.div`
     width: 5rem;
     height: 100vh;
     position: absolute;
-    transition: width 0.5s;
+    transition: width 0.5s, background-color 0.5s;
     display: flex;
     flex-direction: column;
     align-items: center;
+    
+    div:last-child{
+        margin: auto;
+    }
+    :hover{
+        width: 15rem;
+    }
+`;
+export const CustomButton = styled.div`
+    width: 100%;
+    background-color: #252525;
+    transition: background-color 2s;
     img{
         width: 63px;
         height: 63px;
+        margin: 5px 0;
+        transition: filter .7s;
     }
     :hover{
-        width: 10rem;
+        img{
+            filter: invert(48%) sepia(79%) saturate(2476%) hue-rotate(86deg) brightness(118%) contrast(119%);
+        }
+        cursor: pointer;
+        background-color: #171717;
     }
 `;
