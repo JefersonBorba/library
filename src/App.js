4fa49a744e2da5562a6b2cf6ffdd4be127e1bde2
Jeferson <jefersonborba@protonmail.com>
2020-12-22 14:58:59 -0300

    feat: implemented the main routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-import LandingPage from "./Pages/LandingPage/index"
+import LandingPage from "./Pages/LandingPage/index";
+import DetailsPage from "./Pages/DetailsPage/index";
+import SearchPage from "./Pages/SearchPage/index";
+import {Route, Switch} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Switch>
+        <Route>
+          <LandingPage path="/"/>
+        </Route>
+        <Route>
+          <SearchPage path="/search"/>
+        </Route>
+        <Route>
+          <DetailsPage path="/details"/>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/Pages/DetailsPage/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DetailsPage = () => {
+    return <> </>;
+}
+
+export default DetailsPage;
